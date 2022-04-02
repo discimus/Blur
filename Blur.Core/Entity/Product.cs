5e94ca206beeeb3e8f8c59f502d5ece72a7b389f
JonataPereira <jonata@financas360.com.br>
2022-04-02 15:56:08 -0300

    create lib Blur.Core with components

---
+++ Blur.Core/Entity/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blur.Core.Entity
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unity { get; set; }
+        public double UnityPrice { get; set; }
+        public double Total { get; set; }
+    }
+}
---
+++ Blur.Core/Interface/IParserProduct.cs
@@ -0,0 +1,14 @@
+﻿using Blur.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blur.Core.Interface
+{
+    public interface IParserProduct
+    {
+        List<Product> ToProduct();
+    }
+}
