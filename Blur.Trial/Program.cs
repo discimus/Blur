ac024c25f0eec98d6a6071bc7e728e94307213a3
JonataPereira <jonata@financas360.com.br>
2022-04-02 15:57:58 -0300

    create lib Blur.Trial with Program class to verify the app at runtime

---
+++ Blur.Trial/Program.cs
@@ -0,0 +1,36 @@
+﻿using Blur.Core.Entity;
+using Blur.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Blur.Trial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string workingdir = AppDomain.CurrentDomain.BaseDirectory;
+
+            string inputdir = $"{workingdir}..\\..\\..\\Input";
+            string outputdir = $"{workingdir}..\\..\\..\\Output";
+
+            string[] files = Directory.GetFiles(inputdir);
+
+            foreach (string path in files)
+            {
+                using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    FromTxtParser parser = new FromTxtParser(stream);
+                    List<Product> products = parser.ToProduct();
+
+                    foreach (var product in products)
+                    {
+                        Console.WriteLine($"{product.Name} {product.Quantity}");
+                    }
+                }
+            }
+        }
+    }
+}
